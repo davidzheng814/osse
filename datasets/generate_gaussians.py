434f36492b6ba6e2c64bda220b09d77c26753eba
David Zheng <dzd123@mit.edu>
2017-05-15 14:48:04 -0400

    Add Gaussian generator and loader/models.
    1. Update .gitignore to ignore *.npz *.npy files.
    2. Add file to generate gaussians according to a standard linear
    regression model.
    3. Add naive model that predicts y from cur x and last (x,y), but does
    not try to learn an encoding.
    4. Add an encoding model that jointly learns an encoding vector and
    makes a prediction.

---
+++ datasets/generate_gaussians.py
@@ -0,0 +1,52 @@
+'''Generate Gaussian-distributed vectors (X, Y).
+'''
+
+import os
+import sys
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser('Generate Gaussian input-label pairs.')
+parser.add_argument('--output-dir', type=str, default='./gaussian')
+parser.add_argument('--x-size', type=int, default=5)
+parser.add_argument('--y-size', type=int, default=3)
+parser.add_argument('--num-groups', type=int, default=10000)
+parser.add_argument('--num-samples', help="Number of samples per group.", type=int, default=100)
+parser.add_argument('--plot', action="store_true", help="Plot data?")
+parser.add_argument('--add-bias', action="store_true", help="Add bias?")
+parser.add_argument('--add-noise', action="store_true", help="Add noise?")
+args = parser.parse_args()
+
+if args.plot:
+    import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    for i in range(args.num_groups):
+        W = np.random.uniform(low=-20, high=20, size=(args.x_size, args.y_size))
+
+        x = np.random.uniform(low=-10, high=10, size=(args.num_samples, args.x_size))
+        y = np.matmul(x, W)
+
+        if args.add_bias:
+            b = np.random.uniform(low=-20, high=20, size=args.y_size)
+            y += b
+
+        if args.add_noise:
+            y += np.random.normal(loc=0, scale=3, size=y.shape) # Add noise
+
+        if args.plot:
+            plt.plot(y[:,0], y[:,1], 'o')
+        else:
+            output_path = os.path.join(args.output_dir, 'group_'+str(i)+'.npz')
+            if args.add_bias:
+                np.savez(output_path, x_size=args.x_size, y_size=args.y_size,
+                    W=W, b=b, x=x, y=y)
+            else:
+                np.savez(output_path, x_size=args.x_size, y_size=args.y_size,
+                    W=W, x=x, y=y)
+
+    if args.plot:
+        plt.show()
---
+++ models/gaussian/enc_model.py
@@ -0,0 +1,263 @@
+'''Gaussian prediction using encoding.
+'''
+
+import argparse
+import os
+from os.path import join
+import glob
+import numpy as np
+import time
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+""" CONFIG """
+
+ROOT = '../..'
+parser = argparse.ArgumentParser(description='Naive Model for Gaussian Prediction.')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--lr-pred', type=float, default=1e-4,
+                    help='pred model learning rate')
+parser.add_argument('--lr-enc', type=float, default=1e-4,
+                    help='enc model learning rate')
+parser.add_argument('--epochs', type=int, default=10,
+                    help='number of epochs')
+parser.add_argument('--data-dir', type=str, default=join(ROOT, 'datasets/gaussian'),
+                    help='path to training data')
+parser.add_argument('--batch-size', type=int, default=5,
+                    help='batch size')
+parser.add_argument('--max-files', type=int, default=-1,
+                    help='max files to load. (-1 for no max)')
+parser.add_argument('--test-files', type=int, default=50,
+                    help='num files to test.')
+parser.add_argument('--start-train-ind', type=int, default=80,
+                    help='index of each group element to start backproping.')
+parser.add_argument('--start-test-ind', type=int, default=80,
+                    help='index of each group element to start testing.')
+parser.add_argument('--enc-size', type=int, default=25,
+                    help='Size of encoding.')
+parser.add_argument('--weight-ind', type=int, default=-1,
+                    help='If set, print weight matrix of test set at ind')
+parser.add_argument('--use-lstm', action="store_true", help='Use LSTM for Enc Model')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--loss-fn', type=str, default='mse',
+                    help='path to training data')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+args.loss_fn = args.loss_fn.lower()
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+""" DATA LOADERS """
+class GaussianDataset(torch.utils.data.Dataset):
+    def __init__(self, train=True):
+        super(GaussianDataset, self).__init__()
+        self.files = glob.glob(args.data_dir + '/*.npz')
+
+        if args.max_files > 0:
+            self.files = self.files[:args.max_files]
+
+        assert len(self.files) > args.test_files
+
+        if train:
+            self.files = self.files[:-args.test_files]
+        else:
+            self.files = self.files[-args.test_files:]
+
+
+        with np.load(self.files[0]) as data:
+            self.x_size = int(data['x_size'])
+            self.y_size = int(data['y_size'])
+            self.data = [GaussianDataset.to_list(data)]
+            if not train:
+                self.weights = [data['W']]
+
+        for file_ in self.files[1:]:
+            with np.load(file_) as data:
+                assert data['x_size'] == self.x_size and data['y_size'] == self.y_size
+                self.data.append(GaussianDataset.to_list(data))
+                if not train:
+                    self.weights.append(data['W'])
+
+        if not train:
+            self.weights = np.array(self.weights)
+            num_batches = int(math.ceil(len(self.weights) / float(args.batch_size)))
+            self.weights = np.array_split(self.weights, num_batches, axis=0)
+            self.weights = [torch.from_numpy(x.astype(np.float32)).cuda() for x in self.weights]
+
+    @staticmethod
+    def to_list(data):
+        to_torch_tensor = lambda x: torch.from_numpy(x.astype(np.float32))
+        return [(to_torch_tensor(x), to_torch_tensor(y)) for x, y in zip(data['x'], data['y'])]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+
+""" MODELS """
+
+class PredictNet(nn.Module):
+    def __init__(self, enc_size, x_size, y_size):
+        super(PredictNet, self).__init__()
+        self.lin = nn.Linear(enc_size, x_size * y_size)
+
+    def forward(self, x, enc):
+        x = x.unsqueeze(1)
+        W = self.lin(enc).view([-1, x_size, y_size])
+        h = torch.bmm(x, W).squeeze()
+
+        return h, W
+
+
+class EncNet(nn.Module):
+    def __init__(self, enc_size, x_size, y_size):
+        super(EncNet, self).__init__()
+
+        enc_weights = [enc_size] * 3
+        enc_weights = [x_size+y_size+enc_size] + enc_weights
+        # inp_weights = [x_size+y_size] + [enc_size] * 4
+
+        if args.use_lstm:
+            self.lstm1 = nn.LSTMCell(x_size+y_size, enc_size)
+            self.lstm2 = nn.LSTMCell(enc_size, enc_size)
+        else:
+            self.enc_linears = nn.ModuleList([
+                nn.Linear(inp, out) for inp, out in zip(enc_weights[:-1], enc_weights[1:])])
+
+    def forward(self, x, y, enc0):
+        h = torch.cat((x, y, enc0), dim=1)
+        if args.use_lstm: # currently not working
+            # h1, c1 = self.lstm1(xy, (h0, c0))
+            # h2, c2 = self.lstm2(h1, (h1, c1))
+            # return h2, c2
+            pass
+        else:
+            for i, lin in enumerate(self.enc_linears):
+                if i == len(self.enc_linears) - 1: 
+                    h = lin(h)
+                else:
+                    h = F.relu(lin(h))
+
+            return h
+
+
+""" INITIALIZATIONS """
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+train_set = GaussianDataset(train=True)
+train_loader = torch.utils.data.DataLoader(train_set,
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+test_set = GaussianDataset(train=False)
+test_loader = torch.utils.data.DataLoader(test_set,
+    batch_size=args.batch_size, shuffle=False, **kwargs)
+x_size, y_size = train_set.x_size, train_set.y_size
+
+pred_model = PredictNet(args.enc_size, x_size, y_size)
+enc_model = EncNet(args.enc_size, x_size, y_size)
+if args.cuda:
+    pred_model.cuda()
+    enc_model.cuda()
+
+pred_optim = optim.Adam(pred_model.parameters(), lr=args.lr_pred)
+enc_optim = optim.Adam(enc_model.parameters(), lr=args.lr_enc)
+
+mse = nn.MSELoss()
+l1 = nn.L1Loss()
+
+""" HELPERS """
+
+def zero_variable_(size, volatile=False):
+    if args.cuda:
+        return Variable(torch.cuda.FloatTensor(*size).zero_(), volatile=volatile)
+    else:
+        return Variable(torch.FloatTensor(*size).zero_(), volatile=volatile)
+
+""" TRAIN/TEST LOOPS """
+
+def train_epoch(epoch):
+    tot_loss = 0
+    num_samples = 0
+    start_time = time.time()
+    for batch_idx, batch in enumerate(train_loader):
+        batch_loss = zero_variable_((1,))
+        pred_optim.zero_grad()
+        enc_optim.zero_grad()
+
+        batch_size = batch[0][0].size()[0] # get dynamic batch size
+        enc = zero_variable_((batch_size, args.enc_size))
+        encs = []
+
+        for i, (x, y) in enumerate(batch):
+            if args.cuda:
+                x, y = x.cuda(), y.cuda()
+            x, y = Variable(x), Variable(y)
+
+            if i >= args.start_train_ind:
+                pred, W_est = pred_model(x, encs[i-1])
+                if args.loss_fn == 'l1':
+                    loss = l1(pred, y)
+                else:
+                    loss = mse(pred, y)
+
+                batch_loss += loss
+                tot_loss += loss.data[0]
+                num_samples += 1
+
+            enc = enc_model(x, y, enc)
+            encs.append(enc)
+
+        batch_loss.backward()
+        pred_optim.step()
+        enc_optim.step()
+
+    print 'Time: {:.2f}s Epoch: {} Train Loss ({}): {:.3f}'.format(
+        time.time() - start_time, epoch, args.loss_fn.upper(), tot_loss / num_samples)
+
+def test_epoch(epoch):
+    tot_loss = 0
+    W_loss = 0
+    num_samples = 0
+    for batch_idx, batch in enumerate(test_loader):
+        batch_size = batch[0][0].size()[0] # get actual batch size
+        enc = zero_variable_((batch_size, args.enc_size))
+        encs = []
+
+        for i, (x, y) in enumerate(batch):
+            if args.cuda:
+                x, y = x.cuda(), y.cuda()
+            x, y = Variable(x, volatile=True), Variable(y)
+
+            if i >= args.start_test_ind:
+                pred, W = pred_model(x, encs[i-1])
+
+                if i == args.start_test_ind:
+                    W_loss += l1(Variable(test_set.weights[batch_idx]), W).data[0]
+
+                loss = l1(pred, y)
+                tot_loss += loss.data[0]
+                num_samples += 1
+
+            enc = enc_model(x, y, enc)
+            encs.append(enc)
+
+    print 'Test Loss (L1): {:.3f} W Loss (L1): {:.3f}'.format(
+            tot_loss / num_samples, W_loss / (batch_idx+1))
+
+if __name__ == '__main__':
+    for epoch in range(1, args.epochs+1):
+        train_epoch(epoch)
+        test_epoch(epoch)
+
