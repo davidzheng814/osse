37379908acebe03b1b6e51412831596e4348f38d
Vinson Luo <vluo@stanford.edu>
2017-06-22 20:50:27 -0400

    Create datasets.py to house datasets.

---
+++ models/gaussian/datasets.py
@@ -0,0 +1,57 @@
+'''Collection of datasets and a method to construct them.
+'''
+
+import glob
+import numpy as np
+import math
+
+import torch
+import torch.utils.data
+
+class GaussianDataset(torch.utils.data.Dataset):
+    def __init__(self, args, train=True):
+        super(GaussianDataset, self).__init__()
+        self.files = glob.glob(args.data_dir + '/*.npz')
+
+        if args.max_files > 0:
+            self.files = self.files[:args.max_files]
+
+        assert len(self.files) > args.test_files
+
+        if train:
+            self.files = self.files[:-args.test_files]
+        else:
+            self.files = self.files[-args.test_files:]
+
+
+        with np.load(self.files[0]) as data:
+            self.x_size = int(data['x_size'])
+            self.y_size = int(data['y_size'])
+            self.data = [GaussianDataset.to_list(data)]
+            if not train:
+                self.weights = [data['W']]
+
+        for file_ in self.files[1:]:
+            with np.load(file_) as data:
+                assert data['x_size'] == self.x_size and data['y_size'] == self.y_size
+                self.data.append(GaussianDataset.to_list(data))
+                if not train:
+                    self.weights.append(data['W'])
+
+        if not train:
+            self.weights = np.array(self.weights)
+            num_batches = int(math.ceil(len(self.weights) / float(args.batch_size)))
+            self.weights = np.array_split(self.weights, num_batches, axis=0)
+            self.weights = [torch.from_numpy(x.astype(np.float32)).cuda() for x in self.weights]
+
+    @staticmethod
+    def to_list(data):
+        to_torch_tensor = lambda x: torch.from_numpy(x.astype(np.float32))
+        return [(to_torch_tensor(x), to_torch_tensor(y)) for x, y in zip(data['x'], data['y'])]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
--- models/gaussian/enc_model.py
+++ models/gaussian/enc_model.py
@@ -4,10 +4,7 @@
 import argparse
 import os
 from os.path import join
-import glob
-import numpy as np
 import time
-import math
 
 import torch
 import torch.nn as nn
@@ -17,6 +14,7 @@ import torch.utils.data
 from torch.autograd import Variable
 
 import networks
+import datasets
 
 """ CONFIG """
 
@@ -63,61 +61,13 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-""" DATA LOADERS """
-class GaussianDataset(torch.utils.data.Dataset):
-    def __init__(self, train=True):
-        super(GaussianDataset, self).__init__()
-        self.files = glob.glob(args.data_dir + '/*.npz')
-
-        if args.max_files > 0:
-            self.files = self.files[:args.max_files]
-
-        assert len(self.files) > args.test_files
-
-        if train:
-            self.files = self.files[:-args.test_files]
-        else:
-            self.files = self.files[-args.test_files:]
-
-
-        with np.load(self.files[0]) as data:
-            self.x_size = int(data['x_size'])
-            self.y_size = int(data['y_size'])
-            self.data = [GaussianDataset.to_list(data)]
-            if not train:
-                self.weights = [data['W']]
-
-        for file_ in self.files[1:]:
-            with np.load(file_) as data:
-                assert data['x_size'] == self.x_size and data['y_size'] == self.y_size
-                self.data.append(GaussianDataset.to_list(data))
-                if not train:
-                    self.weights.append(data['W'])
-
-        if not train:
-            self.weights = np.array(self.weights)
-            num_batches = int(math.ceil(len(self.weights) / float(args.batch_size)))
-            self.weights = np.array_split(self.weights, num_batches, axis=0)
-            self.weights = [torch.from_numpy(x.astype(np.float32)).cuda() for x in self.weights]
-
-    @staticmethod
-    def to_list(data):
-        to_torch_tensor = lambda x: torch.from_numpy(x.astype(np.float32))
-        return [(to_torch_tensor(x), to_torch_tensor(y)) for x, y in zip(data['x'], data['y'])]
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, key):
-        return self.data[key]
-
 """ INITIALIZATIONS """
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_set = GaussianDataset(train=True)
+train_set = datasets.GaussianDataset(args, train=True)
 train_loader = torch.utils.data.DataLoader(train_set,
     batch_size=args.batch_size, shuffle=True, **kwargs)
-test_set = GaussianDataset(train=False)
+test_set = datasets.GaussianDataset(args, train=False)
 test_loader = torch.utils.data.DataLoader(test_set,
     batch_size=args.batch_size, shuffle=False, **kwargs)
 x_size, y_size = train_set.x_size, train_set.y_size
