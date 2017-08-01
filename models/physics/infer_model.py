58c00dce74b46dfb854d7680387856d6598bc237
Vinson Luo <vluo@stanford.edu>
2017-08-01 00:47:59 -0400

    Add progbar flag, prior, num_sequential_frames.

--- models/physics/infer_model.py
+++ models/physics/infer_model.py
@@ -47,15 +47,17 @@ parser.add_argument('--max-files', type=int, default=-1,
                     help='max files to load. (-1 for no max)')
 parser.add_argument('--test-files', type=int, default=50,
                     help='num files to test.')
-parser.add_argument('--start-train-ind', type=int, default=80,
+parser.add_argument('--start-train-ind', type=int, default=20,
                     help='index of each group element to start backproping.')
-parser.add_argument('--start-test-ind', type=int, default=80,
+parser.add_argument('--start-test-ind', type=int, default=20,
                     help='index of each group element to start testing.')
 parser.add_argument('--weight-ind', type=int, default=-1,
                     help='If set, print weight matrix of test set at ind')
 parser.add_argument('--num-workers', type=int, default=4,
                     help='Num workers to load data')
 parser.add_argument('--use-lstm', action="store_true", help='Use LSTM for Enc Model')
+parser.add_argument('--use-prior', action="store_true", help='Use prior')
+parser.add_argument('--progbar', action="store_true", help='Display progbar')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--no-prior', action='store_true', default=False,
@@ -68,12 +70,20 @@ parser.add_argument('--widths', type=int, default=[50, 50],
                     nargs='+', help='Size of encodings.')
 parser.add_argument('--trans-widths', type=int, default=[25, 25],
                     nargs='+', help='Size of transform layer.')
+parser.add_argument('--num-sequential-frames', type=int, default=4,
+                    help='Number of sequential frames to use for training')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 args.use_prior = not args.no_prior
 args.loss_fn = args.loss_fn.lower()
 args.enc_widths = args.widths
+args.rolling = True
+
+if args.progbar:
+    progbar = tqdm
+else:
+    progbar = lambda x: x
 
 torch.manual_seed(args.seed)
 if args.cuda:
@@ -111,7 +121,7 @@ class PhysicsDataset(torch.utils.data.Dataset):
             self.files = self.files[-args.test_files:]
 
         with np.load(self.files[0]) as data:
-            self.x_size = data['x'].shape[1] * data['x'].shape[2]
+            self.x_size = data['x'].shape[1] * data['x'].shape[2] * args.num_sequential_frames
             self.y_size = data['y'].shape[0]
 
     @staticmethod
@@ -124,7 +134,10 @@ class PhysicsDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, key):
         with np.load(self.files[key]) as data:
-            x = PhysicsDataset.to_list(data['x'])
+            x = []
+            for i in xrange(len(data['x']) - args.num_sequential_frames):
+                x.append(data['x'][i:i+args.num_sequential_frames])
+            x = PhysicsDataset.to_list(x)
             y = data['y']
 
         return x, y
@@ -137,16 +150,16 @@ kwargs = {'num_workers': args.num_workers, 'pin_memory': True} if args.cuda else
 
 start_time = time.time()
 train_set = PhysicsDataset(train=True)
-train_loader = torch.utils.data.DataLoader(train_set,
-    batch_size=args.batch_size, shuffle=True, collate_fn=collate, **kwargs)
+train_loader = torch.utils.data.DataLoader(train_set, collate_fn=collate,
+    batch_size=args.batch_size, shuffle=True, **kwargs)
 test_set = PhysicsDataset(train=False)
-test_loader = torch.utils.data.DataLoader(test_set,
-    batch_size=args.batch_size, shuffle=False, collate_fn=collate,  **kwargs)
+test_loader = torch.utils.data.DataLoader(test_set, collate_fn=collate,
+    batch_size=args.batch_size, shuffle=False,  **kwargs)
 
 x_size = train_set.x_size
 y_size = train_set.y_size
 
-enc_model = networks.ParallelEncNet(args.enc_widths, x_size) # + [1] for mass
+enc_model = networks.ParallelEncNet(args.enc_widths, x_size)
 enc_model_wrapper = networks.get_wrapper('confweight', enc_model, args)
 trans_model = networks.TransformNet(args.enc_widths[-1], args.trans_widths,  y_size)
 if args.cuda:
@@ -178,32 +191,21 @@ def train_epoch(epoch):
     tot_loss = 0
     num_batches = 0
     start_time = time.time()
-    for batch_idx, (x, y) in enumerate(tqdm(train_loader)):
+    for batch_idx, (x, y) in enumerate(progbar(train_loader)):
         enc_optim.zero_grad()
         trans_optim.zero_grad()
-
-        encs, confs = [], []
 
         if args.cuda:
             y = y.cuda()
         y = Variable(y)
 
-        for i, sample in enumerate(x):
-            if args.cuda:
-                sample = sample.cuda()
-            sample = Variable(sample)
-
-            local_enc, conf = enc_model(sample)
-            encs.append(local_enc)
-            confs.append(conf)
-
-        enc_sum = torch.stack(encs).sum(0)[0]
-        conf_sum = torch.stack(confs).sum(0)[0]
-        enc = enc_sum / conf_sum
-        tf_enc = trans_model(enc)
-        loss = mse(tf_enc, y)
-        tot_loss += loss.data[0]
-        num_batches += 1
+        encs = enc_model_wrapper(x)
+        batch_loss = 0
+        for enc in encs[args.start_train_ind:]:
+            tf_enc = trans_model(enc)
+            loss = mse(tf_enc, y)
+            tot_loss += loss.data[0]
+            num_batches += 1
 
         loss.backward()
         enc_optim.step()
@@ -217,29 +219,18 @@ def test_epoch(epoch):
     mse_loss = 0
     num_batches = 0
     start_time = time.time()
-    for batch_idx, (x, y) in enumerate(tqdm(test_loader)):
-        encs, confs = [], []
-
+    for batch_idx, (x, y) in enumerate(progbar(test_loader)):
         if args.cuda:
             y = y.cuda()
         y = Variable(y)
 
-        for i, sample in enumerate(x):
-            if args.cuda:
-                sample = sample.cuda()
-            sample = Variable(sample, volatile=True)
-
-            local_enc, conf = enc_model(sample)
-            encs.append(local_enc)
-            confs.append(conf)
-
-        enc_sum = torch.stack(encs).sum(0)[0]
-        conf_sum = torch.stack(confs).sum(0)[0]
-        enc = enc_sum / conf_sum
-        tf_enc = trans_model(enc)
-        l1_loss += l1(tf_enc, y).data[0]
-        mse_loss += mse(tf_enc, y).data[0]
-        num_batches += 1
+        encs = enc_model_wrapper(x)
+        for enc in encs[args.start_train_ind:]:
+            tf_enc = trans_model(enc)
+            loss = mse(tf_enc, y)
+            l1_loss += l1(tf_enc, y).data[0]
+            mse_loss += mse(tf_enc, y).data[0]
+            num_batches += 1
 
     log('Test Loss (L1): {:.3f} (MSE): {:.3f}'.format(l1_loss / num_batches, mse_loss / num_batches))
 
